7db8c26291822bf8e4f5e57876e32a6adb470926
Joel Weber <hobberwickey@gmail.com>
2015-11-01 10:57:13 -0500

    Switching up the structure

--- lib/polyrhythm.rb
+++ lib/polyrhythm.rb
@@ -1,40 +1,75 @@
 require "polyrhythm/version"
+require 'fileutils'
 
 module Polyrhythm
-  class Setup
-    def self.scaffold
-      create_server
-      create_client
-      create_auth
+  class Scaffold
+    def self.initialize
+      @app_root = Dir.pwd
+      @gem_root = File.expand_path '../..', __FILE__      
+    end
+    
+    def self.init(name, path="/", opts={})
+      require "#{@app_root}/services.rb"
+      
+      services = SERVICES.clone
+      if services[:development][:local][name.to_sym].nil?  && services[:development][:remote][name.to_sym].nil?
+        FileUtils::mkdir_p "#{@app_root}/#{name.downcase}"
+        FileUtils::copy "#{gem_root}/lib/core/config.ru" "#{app_root}/#{name.downcase}/config.ru"
+        services[:development][:local][path] = name.downcase
+
+        File.new("#{@app_root}/services.rb", "w+"){ |f| f.write(
+          "SERVICES = #{Marshal.dump(services)}"
+        ) }
+
+      else
+        puts "Service named #{name} already exists" 
+      end
+    end
+    
+    def self.build_service(name, opts={})
+
     end
 
-    def self.create_server
-      app_root = Dir.pwd
-      gem_root = File.expand_path '../..', __FILE__
+    def self.remote_service(name, opts={})
 
-      FileUtils.cp_r "#{gem_root}/lib/server/.", app_root
     end
 
-    def self.create_client
-      app_root = Dir.pwd
-      gem_root = File.expand_path '../..', __FILE__
 
-      FileUtils.cp_r "#{gem_root}/lib/client/.", app_root
-    end
-
-    def self.create_auth
-      app_root = Dir.pwd
-      gem_root = File.expand_path '../..', __FILE__
-
-      FileUtils.cp_r "#{gem_root}/lib/authorization/.", app_root
-    end
-
-    def self.create_model(name)
-      puts Dir.pwd
-    end
-
-    def self.create_migration(name)
-      puts "NAME: #{name.capitalize}"
-    end
   end
+  # class Setup
+  #   def self.scaffold
+  #     create_server
+  #     create_client
+  #     create_auth
+  #   end
+
+  #   def self.create_server
+  #     app_root = Dir.pwd
+  #     gem_root = File.expand_path '../..', __FILE__
+
+  #     FileUtils.cp_r "#{gem_root}/lib/server/.", app_root
+  #   end
+
+  #   def self.create_client
+  #     app_root = Dir.pwd
+  #     gem_root = File.expand_path '../..', __FILE__
+
+  #     FileUtils.cp_r "#{gem_root}/lib/client/.", app_root
+  #   end
+
+  #   def self.create_auth(path="")
+  #     app_root = "#{Dir.pwd}path"
+  #     gem_root = File.expand_path '../..', __FILE__
+
+  #     FileUtils.cp_r "#{gem_root}/lib/authorization/.", app_root
+  #   end
+
+  #   def self.create_model(name)
+  #     puts Dir.pwd
+  #   end
+
+  #   def self.create_migration(name)
+  #     puts "NAME: #{name.capitalize}"
+  #   end
+  # end
 end
--- lib/server/lib/helpers.rb
+++
@@ -1,8 +0,0 @@
-class Application < Sinatra::Base
-  helpers do
-    def current_user
-      return unless session[:user_id]
-      @current_user ||= User.find(session[:user_id])
-    end
-  end
-end
