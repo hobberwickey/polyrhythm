89688171021241f9033c9d892c9c7bff41038cd1
Joel Weber <hobberwickey@gmail.com>
2015-11-01 23:10:06 -0500

    Adding in service generating files

--- lib/polyrhythm.rb
+++ lib/polyrhythm.rb
@@ -1,5 +1,6 @@
 require "polyrhythm/version"
 require 'fileutils'
+require "json"
 
 module Polyrhythm
   class Scaffold
@@ -10,38 +11,98 @@ module Polyrhythm
       }
     }
 
-    def self.initialize
+    DEFAULT_CONFIG = {
+      :db_roots => {
+        :pg => "postgres",
+      },  
+
+      :db_ports => {
+        :pg => "5432"
+      },
+
+      :db_user => "DB_USERNAME",
+      :db_pass => "DB_PASSWORD",
+      :db_name => "DB_NAME",
+      :db_host => "localhost",
+      :db_port => nil,
+      :db_type => "pg"
+    }
+
+    def initialize
       @app_root = Dir.pwd
       @gem_root = File.expand_path '../..', __FILE__      
     end
     
-    def self.init(name, path="/", opts={})
-      services = DEFAULT_SERVICES.clone
+    def init(name, path='/', opts={})
+      @name = name
+      @path = path
+      @config = DEFAULT_CONFIG.merge(opts)
+
+      @services = DEFAULT_SERVICES.clone
+      FileUtils::copy "#{@gem_root}/lib/core/config.ru", "#{@app_root}/config.ru"
       
-      if services[:development][:local][name.to_sym].nil?  && services[:development][:remote][name.to_sym].nil?
-        FileUtils::mkdir_p "#{@app_root}/#{name.downcase}"
-        FileUtils::copy "#{gem_root}/lib/core/config.ru" "#{app_root}/#{name.downcase}/config.ru"
-        services[:development][:local][path] = name.downcase
-
-        File.new("#{@app_root}/services.rb", "w+"){ |f| f.write(
-          "SERVICES = #{Marshal.dump(services)}"
-        ) }
-
-      else
-        puts "Service named #{name} already exists" 
-      end
+      write_services
+      build_service(nil, @path, opts)
     end
     
-    def self.build_service(name, opts={})
+    def build_service(name, path, opts={})
+      unless defined? @name 
+        require "#{app_root}/services"
+
+        @name = name
+        @config = DEFAULT_CONFIG.merge(opts)
+        @services = SERVICES
+        write_services
+      end
+
+      unless path.nil?
+        @path = path
+      else
+        #error
+      end
+
+      if defined? @name 
+        dir = "#{@app_root}/#{@name.downcase}"
+        FileUtils.cp_r "#{@gem_root}/lib/service/.", dir
+
+        File.open("#{dir}/.env", 'w+'){ |f| f.write (
+          erb "#{@gem_root}/templates/env"
+        )}
+
+        File.open("#{dir}/Gemfile", 'w+'){ |f| f.write (
+          erb "#{@gem_root}/templates/Gemfile"
+        )}
+
+        File.open("#{dir}/application.rb", 'w+'){ |f| f.write (
+          erb "#{@gem_root}/templates/application"
+        )}
+      else 
+        #error
+      end
 
     end
 
-    def self.remote_service(name, opts={})
+    def remote_service(name, opts={})
 
     end
 
+    private
 
+    def write_services
+      #TODO: better checks
+      if @services[:development][:local][@path].nil?  && @services[:development][:remote][@path].nil?
+        @services[:development][:local][@path] = @name.downcase
+
+        File.open("#{@app_root}/services.rb", "w+"){ |f| f.write(
+          "SERVICES = #{@services.to_s}"
+        )}
+      else
+        
+      end
+    end
   end
+
+
   # class Setup
   #   def self.scaffold
   #     create_server
---
+++ lib/service/application.rb
@@ -0,0 +1,31 @@
+require "rubygems"
+require "bundler"
+require "active_record"
+
+require "sinatra/base"
+require "sinatra/json"
+require "sinatra/cookies"
+
+require "json"
+require "yaml"
+
+require 'dotenv'
+
+Dotenv.load
+
+Bundler.require :default, (ENV["RACK_ENV"] || "development").to_sym
+ActiveRecord::Base.raise_in_transactional_callbacks = true # To silence all those stupid warnings
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+
+Dir[File.dirname(__FILE__) + "/models/*.rb"].each { |file| require file }
+
+class Application < Sinatra::Base  
+  helpers Sinatra::Cookies
+
+  get "/" do
+    "Welcome to your service"
+  end
+end
+
+Dir[File.dirname(__FILE__) + "/lib/*.rb"].each { |file| require file }
+Dir[File.dirname(__FILE__) + "/lib/formats/*.rb"].each { |file| require file }
